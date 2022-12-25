e4a2ac5035f609939decfad4a99c39b994c27ea7
Abhishek <abhisharma05052002@gmail.com>
2022-12-25 18:54:53 +0530

    ifandelse

---
+++ page8.js
@@ -0,0 +1,11 @@
+let male=21;
+let female=18;
+if((gender==="male")&&(male===21)||(gender==="female")&&(female>=18))
+{
+  console.log("Elegible");
+}
+else
+{
+  console.log("Not Elegible");
+}
+((gender==="male")&&(male>=21)||(gender==="female")&&(female>=18))?console.log("yes");:console.log("not");
